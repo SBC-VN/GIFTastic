25a032954e2f08f05db45b2c2193daf6990ead0a
SBC-VN <vnestohr@juno.com>
2019-03-18 23:03:58 -0400

    added persistance of user added topics, and removal of topic (button + gifs)

--- assets/javascript/giftastic.js
+++ assets/javascript/giftastic.js
@@ -1,22 +1,49 @@
 var topics = ["goat","chicken","cow","sheep","pig","cat","dog"];
 
-function addButton(buttonName) {
+function addButton(topic) {
+    var buttonName = topic.toLowerCase()
+    var newButtonDiv = $("<div class='btn-div btn-group'>");
+    newButtonDiv.attr("data-topic",buttonName);
     var newButton = $("<button type='button' class='btn btn-info topic-button'>");
-    newButton.text(buttonName.toLowerCase());
-    newButton.attr("data-topic",buttonName.toLowerCase());
+    newButton.text(buttonName);
+    newButton.attr("data-topic",buttonName);
     newButton.attr("data-set",0);
-    $("#button-section").append(newButton);
+    var newButtonRemove = $("<button type='button' class='btn btn-danger topic-remove-button'>");
+    newButtonRemove.text("X");
+    newButtonRemove.attr("data-topic",buttonName);
+    newButtonDiv.append(newButton);
+    newButtonDiv.append(newButtonRemove);
+    $("#button-section").append(newButtonDiv);
 }
 
 function addTopic() {
     if ($("#add-animal").val()) {
         addButton($("#add-animal").val());
+        var prevButtonStore=localStorage.getItem("user-buttons");
+
+        if (prevButtonStore != undefined) {
+            prevButtonStore += $("#add-animal").val() + ",";
+        }
+        
+        localStorage.setItem("user-buttons",prevButtonStore);
     }
 }
 
 function setInitialButtonSet() {
     for (var i=0; i<topics.length; i++) {
         addButton(topics[i]);
+    }
+    var prevButtonStore=localStorage.getItem("user-buttons");
+    
+    if (prevButtonStore) {
+        var storedTopics = prevButtonStore.split(",");
+
+        for (var i=0; i<storedTopics.length; i++) {
+            // Filter out blank spaces.
+            if (storedTopics[i]) {
+                addButton(storedTopics[i]);
+            }
+        }
     }
 }
 
@@ -36,13 +63,41 @@ function addGif(gifInfo,topic) {
     $("#gif-section").prepend(gifContainer);
 }
 
+function removeTopic(topic) {
+
+    var prevButtonStore=localStorage.getItem("user-buttons");
+    if (prevButtonStore != undefined) {
+        var storedTopics = prevButtonStore.split(",");
+        prevButtonStore = "";
+        for (var i=0; i<storedTopics.length; i++) {
+            if (storedTopics[i] != topic) {
+                if (storedTopics[i]) {
+                    prevButtonStore += storedTopics[i] + ",";
+                }
+            }      
+        }
+        localStorage.setItem("user-buttons",prevButtonStore);
+    }
+
+    // Remove anything with the topic from the display.  Buttons & gifs.
+    topicGifs = $("[data-topic="+topic+"]");
+    for (var i=0; i<topicGifs.length; i++) {
+        topicGifs[i].remove();
+    }
+}
+
 setInitialButtonSet();
 
 $("#button-section").on("click","button",function() {
     var topic = $(this).attr("data-topic");
     var dataSet = parseInt($(this).attr("data-set"));
-    console.log(dataSet);
-    console.log(typeof dataSet);
+
+    // Check if they clicked on the remove button...
+    if (this.className.indexOf("topic-remove-button") >= 0) {
+        removeTopic(topic);
+        return;
+    }
+    
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
         topic + "&api_key=dc6zaTOxFJmzC&limit=10&rating=g&offset=" + dataSet;
     dataSet += 10;
@@ -57,6 +112,7 @@ $("#button-section").on("click","button",function() {
                 }
         });
 });
+
 
 $("#gif-section").on("click","img",function() {
     var state = $(this).attr("data-state");
